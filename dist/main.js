8342930cea18059ad6914cfebc4bd35500abf8ae
aama012026 <aama012026@gmail.com>
2026-02-23 03:04:04 +0100

    Created tab view functionality in ts and changed fallback colors to something more readable for unfinished landing page.

--- dist/main.js
+++ dist/main.js
@@ -1,7 +1,60 @@
-const stilCss = await getFileContent('../assets/css/stil.css');
-const coderHtml = await getFileContent('../pages/coder.html');
+const container = queryExistingElement('.tabline');
+const codeElements = {
+    coderHtml: {
+        sourceText: await getFileContent('/pages/coder.html'),
+        button: queryExistingElement('#btn-tab-html'),
+        view: queryExistingElement('#coder-html'),
+        isActive: true
+    },
+    stilCss: {
+        sourceText: await getFileContent('/assets/css/stil.css'),
+        button: queryExistingElement('#btn-tab-css'),
+        view: queryExistingElement('#stil-css'),
+        isActive: false
+    },
+    mainTs: {
+        sourceText: await getFileContent('/src/main.ts'),
+        button: queryExistingElement('#btn-tab-ts'),
+        view: queryExistingElement('#main-ts'),
+        isActive: false
+    }
+};
+renderSourceCode();
+showActiveTabsOnly();
+container.addEventListener('pointerdown', (e) => {
+    Object.keys(codeElements).forEach(key => {
+        codeElements[key].isActive = (codeElements[key].button === e.target);
+    });
+    showActiveTabsOnly();
+});
 async function getFileContent(path) {
     return await fetch(path).then(r => r.text());
+}
+function queryExistingElement(selector) {
+    const element = document.querySelector(selector);
+    if (!element) {
+        throw new TypeError(`Could not find element for ${selector}. Element is null!`);
+    }
+    return element;
+}
+function renderSourceCode() {
+    Object.keys(codeElements).forEach(key => {
+        codeElements[key].view.textContent = codeElements[key].sourceText;
+        Prism.highlightElement(codeElements[key].view);
+    });
+}
+function showActiveTabsOnly() {
+    Object.keys(codeElements).forEach(key => {
+        const codePage = codeElements[key];
+        if (codePage.isActive) {
+            codePage.view.parentElement.classList.remove('visually-hidden');
+            codePage.button.classList.add('is-active');
+        }
+        else {
+            codePage.view.parentElement.classList.add('visually-hidden');
+            codePage.button.classList.remove('is-active');
+        }
+    });
 }
 export {};
 //# sourceMappingURL=main.js.map
